c511aa5b1bd3052667729406d2b34b3bf3145776
matthewclouthier <mclouthier@wisc.edu>
2022-02-17 22:41:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-f8gm8y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-f8gm8y)
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <Base />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Base from './components/Base.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Base
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
